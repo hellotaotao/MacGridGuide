51176b58c513476c739d6447e09e478c8c85ce84
Tao Wang <hellotaotao@gmail.com>
2024-02-14 23:04:10 +1030

    Initial Commit

---
+++ GridGuide/GridGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridGuideApp.swift
+//  GridGuide
+//
+//  Created by Tao Wang on 14/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct GridGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
